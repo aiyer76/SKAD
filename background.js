1423ac614302a3aad6c0b0aa85ef510c9117abe9
Garimella, Sumedh Surya <sgarimella30@gatech.edu>
2019-09-07 19:38:57 -0400

    UI is working finally! yeet

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
+
 chrome.runtime.onInstalled.addListener(function() {
     console.log('Loaded successfully.');
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'www.bbc.com'},
+          pageUrl: {hostContains: ''},
         })
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
--- toggles.js
+++ toggles.js
@@ -1,8 +1,8 @@
-function vocabToggle() {
-    var toggled = document.getElementById('customSwitch1').checked;
-    if(toggled) {
-        console.log('off');
-    } else {
-        console.log('on');
-    }
-}
+document.getElementById('customSwitch1').addEventListener("change", function() {
+        var toggled = document.getElementById('customSwitch1').checked;
+        if(toggled) {
+            console.log("off");
+        } else {
+            text = "Ukraine and Russia exchange prisoners in landmark deal"
+        }
+    });
