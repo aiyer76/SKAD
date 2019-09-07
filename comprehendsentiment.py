ebb1f9d694271f1a385b9b0e8d87cf0b6c8202d4
Garimella, Sumedh Surya <sgarimella30@gatech.edu>
2019-09-07 17:14:21 -0400

    added aws sdk

--- comprehendsentiment.py
+++ comprehendsentiment.py
@@ -3,7 +3,7 @@ import json
 
 comprehend = boto3.client(service_name='comprehend', region_name='region')
 
-text = "It is raining today in Seattle"
+text = "Ukraine and Russia exchange prisoners in landmark deal"
 
 print('Calling DetectSentiment')
 print(json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
