dbc97d40854a64d59675d96a04e1533a0b187126
Garimella, Sumedh Surya <sgarimella30@gatech.edu>
2019-09-07 13:22:00 -0400

    added JS for the toggle

---
+++ toggles.js
@@ -0,0 +1,8 @@
+function vocabToggle() {
+    var toggled = document.getElementById('customSwitch1').checked;
+    if(toggled) {
+        console.log('off');
+    } else {
+        console.log('on');
+    }
+}
