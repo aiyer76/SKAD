635387395cc4120b02ea726c6e325ae17bcb01ef
Garimella, Sumedh Surya <sgarimella30@gatech.edu>
2019-09-08 03:04:39 -0400

    starting aws implementation

--- toggles.js
+++ toggles.js
@@ -9,6 +9,11 @@ document.getElementById('customSwitch1').addEventListener("change", function() {
                     tabs[0].id,
                     {code: 'document.getElementsByTagName("h1")[0].style.backgroundColor = "yellow";'});
               });
+              chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.executeScript(
+                    tabs[0].id,
+                    {code: 'console.log(aws.detectSentiment("en", document.getElementsByTagName("h1")[0].innerText));'});
+              });
             /*
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {scrape: true}, function(response) {
